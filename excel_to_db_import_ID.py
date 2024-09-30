b77f578be89819af75beb2c14acd1c5bec510f92
Patty <pcoglan06@gmail.com>
2024-09-30 16:34:47 +1000

    Bug Fixes / Error Checking for IDs that already exist
    Some Error checking to complete the script. This will allow no duplicate IDs of BOTH secondaryTurtleIDs AND secondaryImageIDs.
    
    + Updated scraping filenames to be more clear of purpose
    + Added duplicate checking for both secondaryIDs in their functions
    + If it doesnt exist yet, insert. If it does then do not insert the duplicate.
    + Instead of reading the file again a second time to insert ImageIDs we appended them into a tuple the first time around. We will insert values from the tuple into the table.
    
    Note: Two package.json were updated in this commit not by me. This was done AUTOMATICALLY. This is to log the change. Not sure if it is important or if it will impact other developers.

--- excel_to_db_import_ID.py
+++ excel_to_db_import_ID.py
@@ -27,35 +27,59 @@ def read_excel_file(file_path, sheet_name):
         return None
 
 def insert_s_turtleID(df, connection):
+    # SQL INSERT STATEMENTS
     insert_sql_secondaryTurtle = """INSERT INTO Turtle (secondaryTurtleID) VALUES (?);"""
-    get_last_identityID = """SELECT SCOPE_IDENTITY();"""
+    get_Identity_ID = """SELECT TurtleID FROM Turtle WHERE secondaryTurtleID = ?;""" # Get the IDENTITY turtleID value
+    check_duplicate_turtleID = """SELECT COUNT(*) FROM Turtle WHERE secondaryTurtleID = ?;"""  # Check for duplicates
 
     # Iterate over the DataFrame and insert values into the Turtle table
-    turtle_ids = []
+    identity_image_tuple = []
     try:
         with connection.cursor() as cursor:
-            for i, row in df.itertuples(index=False):
-                cursor.execute(insert_sql_secondaryTurtle, row['turtle_id'])
-                cursor.execute(get_last_identityID) 
-                current_turtle_id = cursor.fetchone()[0]
-                turtle_ids.append(current_turtle_id)
+            for row in df.itertuples(index=False):
+                cursor.execute(check_duplicate_turtleID, row.turtle_id)
+                exists = cursor.fetchone()[0]
+
+                
+                # If the turtleID does not exist in the Turtle table, insert it
+                if exists == 0:
+                    cursor.execute(insert_sql_secondaryTurtle, row.turtle_id)
+                    cursor.execute(get_Identity_ID, row.turtle_id)   
+                    current_turtle_id = cursor.fetchone()[0]
+                # If the turtleID exists in the Turtle table, dont insert it
+                else:
+                    cursor.execute(get_Identity_ID, row.turtle_id)   
+                    current_turtle_id = cursor.fetchone()[0]
+
+                # NOTE: Appending image_id with most recent IDENTITY turtleID regardless 
+                identity_image_tuple.append((current_turtle_id, row.image_id))
+                # print(f"Inserted Tuple: {current_turtle_id, row.image_id}")
         connection.commit()
+
     except pyodbc.Error as e:
         print(f"Error inserting into Turtle table: {e}")
         connection.rollback()
 
     # Return IDENTITY turtleID values to be used as a parameter for 'insert_s_imageID' function
-    return turtle_ids  
+    return identity_image_tuple 
 
 
-def insert_s_imageID(df, turtle_ids, connection):
+def insert_s_imageID(identity_image_tuple, connection):
     insert_sql_secondaryImage = """INSERT INTO Image (turtleID, secondaryImageID) VALUES (?, ?);"""
+    check_duplicate_imageID = """SELECT COUNT(*) FROM Image WHERE secondaryImageID = ?;"""  # Check for duplicates
 
     # Insert excel imageID and their respective turtleID into the Image table
     try:
         with connection.cursor() as cursor:
-            for i, row in df.iterrows():
-                cursor.execute(insert_sql_secondaryImage, turtle_ids[i], row['image_id'])
+            for turtle_id, image_id in identity_image_tuple:
+                cursor.execute(check_duplicate_imageID, image_id)
+                exists = cursor.fetchone()[0]
+
+                if exists == 0:
+                    cursor.execute(insert_sql_secondaryImage, turtle_id, image_id)
+                else:
+                    # print(f"Image ID: {image_id} already exists in the Image table.")
+                    pass
         connection.commit() 
     except pyodbc.Error as e:
         print(f"Error inserting into Image table: {e}")
@@ -72,11 +96,9 @@ def main():
         df = read_excel_file(file_path, sheet_name=sheet_name)
         if df is not None:
             turtle_ids = insert_s_turtleID(df, connection)
-            insert_s_imageID(df, turtle_ids, connection)
+            insert_s_imageID(turtle_ids, connection)
 
-            # Display the DataFrame
             connection.commit()
-            print(df)
     except Exception as e:
         print(f"Error: {e}")
     finally:
@@ -86,6 +108,6 @@ if __name__ == "__main__":
     main()    
 
 # TO DO
-# 1. CHANGE FILE PATH TO CORRECT FILE PATH OF EXCEL FILE (LINE 65)
-# 2. CHECK SHEET NAME AND COLUMN HEADERS ARE CORRECT (LINE 22, 23, 38, 58)
+# 1. CHANGE FILE PATH TO CORRECT FILE PATH OF EXCEL FILE (LINE 89)
+# 2. CHECK SHEET NAME AND COLUMN HEADERS ARE CORRECT (LINE 22, 23, 90)
 # 3. RUN THE SCRIPT
