d546454a6a9bf3532df8dc6c5234a8896ae6b17c
Tom Campbell <65591392+tomcampbell23@users.noreply.github.com>
2024-10-10 18:48:55 +1000

    Styles update

--- industry-app/src/routes/+page.svelte
+++ industry-app/src/routes/+page.svelte
@@ -138,7 +138,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>TRAC Application</title>
-    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="./src/styles.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
 </head>
 
